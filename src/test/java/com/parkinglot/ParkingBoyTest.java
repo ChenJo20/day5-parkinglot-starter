4896017b99aa189355b88b8406568c9ed560e206
CHENJO20 <CHENJO20@oocl.com>
2024-11-22 15:23:00 +0800

    [Story3] feat, case3, should_return_right_car_when_fetch_given_two_tickets

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
@@ -26,5 +27,21 @@ public class ParkingBoyTest {
         Car fetchedCar = parkingBoy.fetch(ticket);
         // Then
         assertNotNull(fetchedCar);
+    }
+
+    @Test
+    void should_return_right_car_when_fetch_given_two_tickets(){
+        // Given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket ticket2 = parkingBoy.park(car2);
+        // When
+        Car fetchedCar1 = parkingBoy.fetch(ticket1);
+        Car fetchedCar2 = parkingBoy.fetch(ticket2);
+        // Then
+        assertEquals(fetchedCar1, car1);
+        assertEquals(fetchedCar2, car2);
     }
 }
