fefd648021771576f237bcabaa428d02136ba30b
CHENJO20 <CHENJO20@oocl.com>
2024-11-22 15:20:19 +0800

    [Story3] feat, case2, should_return_ticket_when_park_given_a_car_and_a_parking_lot

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -9,11 +9,22 @@ public class ParkingBoyTest {
     void should_return_ticket_when_park_given_a_car_and_a_parking_lot(){
         // Given
         ParkingBoy parkingBoy = new ParkingBoy();
-        ParkingLot parkingLot = new ParkingLot();
         Car car = new Car();
         // When
         Ticket ticket = parkingBoy.park(car);
         // Then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_car_when_fetch_given_and_a_ticket(){
+        // Given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car = new Car();
+        Ticket ticket = parkingBoy.park(car);
+        // When
+        Car fetchedCar = parkingBoy.fetch(ticket);
+        // Then
+        assertNotNull(fetchedCar);
     }
 }
