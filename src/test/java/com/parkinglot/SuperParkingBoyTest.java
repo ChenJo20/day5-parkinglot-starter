a033f215e1cbfb389f25246586d1b798b25ff82b
CHENJO20 <CHENJO20@oocl.com>
2024-11-22 18:01:34 +0800

    [Story6] feat, case2, should_park_in_lot2_when_park_given_a_car_and_lot1_20_capacity_10_available_and_lot2_10_capacity_10_available

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -26,4 +26,22 @@ public class SuperParkingBoyTest {
         assertNotNull(ticket);
         assertEquals(fetchedCar, car);
     }
+
+    @Test
+    void should_park_in_lot1_when_park_given_a_car_and_lot1_20_capacity_10_available_and_lot2_10_capacity_5_available() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(20);
+        IntStream.range(0, 10).forEach(i -> parkingLot1.park(new Car()));
+        ParkingLot parkingLot2 = new ParkingLot();
+        IntStream.range(0, 5).forEach(i -> parkingLot2.park(new Car()));
+        List<ParkingLot> parkingLots = Arrays.asList(parkingLot1, parkingLot2);
+        SuperParkingBoy parkingBoy = new SuperParkingBoy(parkingLots);
+        Car car = new Car();
+        // When
+        Ticket ticket = parkingBoy.park(car);
+        Car fetchedCar = parkingLot1.fetch(ticket);
+        // Then
+        assertNotNull(ticket);
+        assertEquals(fetchedCar, car);
+    }
 }
