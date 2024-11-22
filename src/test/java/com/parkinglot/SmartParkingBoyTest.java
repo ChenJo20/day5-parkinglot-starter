99d08c3323db2fbd9a92804ae30e33c066091144
CHENJO20 <CHENJO20@oocl.com>
2024-11-22 17:42:35 +0800

    [Story5] feat, case3, should_return_right_car_when_fetch_given_two_cars_and_two_lots_and_each_lot_has_one_car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -26,20 +27,25 @@ public class SmartParkingBoyTest {
     }
 
     @Test
-    void should_park_in_lot2_when_park_given_a_car_and_lot1_9_available_positions_and_lot2_10_available_positions() {
+    void should_return_right_car_when_fetch_given_two_cars_and_two_lots_and_each_lot_has_one_car() {
         // Given
         ParkingLot parkingLot1 = new ParkingLot();
-        parkingLot1.park(new Car());
+
         ParkingLot parkingLot2 = new ParkingLot();
         List<ParkingLot> parkingLots = Arrays.asList(parkingLot1, parkingLot2);
         SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car carInLot1 = new Car();
+        Ticket ticketForFirstCarInLot1 = smartParkingBoy.park(carInLot1);
+        IntStream.rangeClosed(0, 8).forEach(i -> parkingLot1.park(new Car()));
+        Car carInLot2 = new Car();
+        Ticket ticketForFirstCarInLot2 = smartParkingBoy.park(carInLot2);
 
         // When
-        Car car = new Car();
-        Ticket ticket = smartParkingBoy.park(car);
-        Car fetchedCar = parkingLot2.fetch(ticket);
+        Car fetchedCarInLot2 = smartParkingBoy.fetch(ticketForFirstCarInLot2);
+        Car fetchedCarInLot1 = smartParkingBoy.fetch(ticketForFirstCarInLot1);
         // Then
-        assertEquals(fetchedCar, car);
+        assertEquals(fetchedCarInLot2, carInLot2);
+        assertEquals(fetchedCarInLot1, carInLot1);
     }
 
     @Test
