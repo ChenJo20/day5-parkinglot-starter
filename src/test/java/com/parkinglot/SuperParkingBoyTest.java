7764d3cb510b096998f05b33b48568514faee608
CHENJO20 <CHENJO20@oocl.com>
2024-11-24 09:32:17 +0800

    [Story6] fix, case4, should use superParkingBoy instead of smartParkingBoy

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -75,7 +75,7 @@ public class SuperParkingBoyTest {
         ParkingLot parkingLot1 = new ParkingLot();
         ParkingLot parkingLot2 = new ParkingLot();
         List<ParkingLot> parkingLots = Arrays.asList(parkingLot1, parkingLot2);
-        SmartParkingBoy parkingBoy = new SmartParkingBoy(parkingLots);
+        SuperParkingBoy parkingBoy = new SuperParkingBoy(parkingLots);
         Ticket wrongTicket = new Ticket();
 
         // When
