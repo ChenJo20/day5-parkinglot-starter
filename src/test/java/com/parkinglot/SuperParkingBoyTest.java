0c6fd9d99346cf4c54b16fc30559de092b80aefb
CHENJO20 <CHENJO20@oocl.com>
2024-11-24 09:20:10 +0800

    [Story6] feat, case3, should_fetch_right_car_when_park_given_two_cars_and_lot1_20_capacity_10_available_and_lot2_10_capacity_5_available

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -44,4 +44,27 @@ public class SuperParkingBoyTest {
         assertNotNull(ticket);
         assertEquals(fetchedCar, car);
     }
+
+    @Test
+    void should_fetch_right_car_when_park_given_two_cars_and_lot1_20_capacity_10_available_and_lot2_10_capacity_5_available() {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(20);
+        IntStream.range(0, 10).forEach(i -> parkingLot1.park(new Car()));
+        ParkingLot parkingLot2 = new ParkingLot();
+        IntStream.range(0, 5).forEach(i -> parkingLot2.park(new Car()));
+        List<ParkingLot> parkingLots = Arrays.asList(parkingLot1, parkingLot2);
+        SuperParkingBoy parkingBoy = new SuperParkingBoy(parkingLots);
+        Car carInLot1 = new Car();
+        Car carInLot2 = new Car();
+        // When
+        Ticket ticketForCar1 = parkingBoy.park(carInLot1);
+        Ticket ticketForCar2 = parkingBoy.park(carInLot2);
+        Car fetchedCar1 = parkingLot1.fetch(ticketForCar1);
+        Car fetchedCar2 = parkingLot2.fetch(ticketForCar2);
+        // Then
+        assertNotNull(ticketForCar1);
+        assertNotNull(ticketForCar2);
+        assertEquals(fetchedCar1, carInLot1);
+        assertEquals(fetchedCar2, carInLot2);
+    }
 }
